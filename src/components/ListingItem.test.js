cff21bf5edd57718af6f885b3a905871d2333c3f
Brooks Lyrette <brooks@dotsub.com>
2016-09-10 08:36:24 -0400

    Fixed broken test

--- src/components/ListingItem.test.js
+++ src/components/ListingItem.test.js
@@ -6,6 +6,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ListingItem index={0} item={{
     thumbnail: '',
+    created_utc: 0,
     url: '',
     title: '',
   }}/>, div);
