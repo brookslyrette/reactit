7233a8220110a5d7a51e582c61ede5debbe771e4
Brooks Lyrette <brooks@dotsub.com>
2016-09-18 10:59:50 -0400

    Updated action creators to use more es6 syntax

--- src/actions/redditActionCreators.js
+++ src/actions/redditActionCreators.js
@@ -6,64 +6,61 @@ export const CHANGE_TYPE = 'CHANGE_TYPE';
 export const DEFAULT_REDDITS = 'DEFAULT_REDDITS';
 export const EXPAND_ITEM = 'EXPAND_ITEM';
 
-export function changeType(type) {
-  return (dispatch) => dispatch({
-      type: CHANGE_TYPE,
-      newType: type,
-    });
-}
+export const changeType = (type) => (dispatch) =>
+  dispatch({
+    type: CHANGE_TYPE,
+    newType: type,
+  });
 
-export function expandItem(item) {
-  return (dispatch) => dispatch({
-      type: EXPAND_ITEM,
-      item,
-    });
-}
+export const expandItem = (item) => (dispatch) => dispatch({
+    type: EXPAND_ITEM,
+    item,
+  });
 
 
-export function loadReddit(type = 'hot') {
-  return (dispatch) => fetch(`https://www.reddit.com/${type}.json`)
+export const loadReddit = (type = 'hot') => (dispatch) =>
+  fetch(`https://www.reddit.com/${type}.json`)
     .then(response => response.json())
     .then(json => dispatch({
       type: LOAD_SUBREDDIT,
       data: json.data,
       reddit: 'Front Page',
-    }))
-}
+    })
+  );
 
-export function loadMoreItems(type = 'hot', after) {
-  return (dispatch) => fetch(`https://www.reddit.com/${type}.json?after=${after}`)
+export const loadMoreItems = (type = 'hot', after) => (dispatch) =>
+  fetch(`https://www.reddit.com/${type}.json?after=${after}`)
     .then(response => response.json())
     .then(json => dispatch({
       type: LOAD_MORE_ITEMS,
       data: json.data,
-    }))
-}
+    })
+  );
 
-export function loadSubreddit(name, type = 'hot') {
-  return (dispatch) => fetch(`https://www.reddit.com/r/${name}/${type}.json`)
+export const loadSubreddit = (name, type = 'hot') => (dispatch) =>
+  fetch(`https://www.reddit.com/r/${name}/${type}.json`)
     .then(response => response.json())
     .then(json => dispatch({
       type: LOAD_SUBREDDIT,
       data: json.data,
       reddit: name,
-    }))
-}
+    })
+  );
 
-export function loadMoreSubredditItems(name, type = 'hot', after) {
-  return (dispatch) => fetch(`https://www.reddit.com/r/${name}/${type}.json?after=${after}`)
+export const loadMoreSubredditItems = (name, type = 'hot', after) => (dispatch) =>
+  fetch(`https://www.reddit.com/r/${name}/${type}.json?after=${after}`)
     .then(response => response.json())
     .then(json => dispatch({
       type: LOAD_MORE_ITEMS,
       data: json.data,
-    }))
-}
+    })
+  );
 
-export function loadDefaultReddits() {
-  return (dispatch) => fetch('https://www.reddit.com/subreddits/default.json?limit=15')
+export const loadDefaultReddits = () => (dispatch) =>
+  fetch('https://www.reddit.com/subreddits/default.json?limit=15')
     .then(response => response.json())
     .then(json => dispatch({
       type: DEFAULT_REDDITS,
       data: json.data,
-    }))
-}
+    })
+  );
