1a8adb231ad63f5083eb12e39ccd388e107567b3
Brooks Lyrette <brooks@dotsub.com>
2016-10-02 07:59:15 -0400

    Added an appear transition

--- src/components/ListingItem.js
+++ src/components/ListingItem.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
 import ReactMarkdown from 'react-markdown';
-
-import TimeAgo from 'react-timeago'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import TimeAgo from 'react-timeago';
 
 export default class ListingItem extends Component {
 
@@ -61,36 +61,37 @@ export default class ListingItem extends Component {
 
   render() {
     return (
-      <div className="row">
-        <div className="col-md-1">
-          <div className="row">
-            <div className="col-md-1 item-index">
-              {this.props.index + 1}
-            </div>
-            <div className="col-md-1 item-vote">
-              ⇧ <br/>
-              {this.props.item.score} <br/>
-              ⇩ <br/>
+      <ReactCSSTransitionGroup transitionName="items" transitionAppear={true} transitionAppearTimeout={300}>
+        <div className="row">
+          <div className="col-md-1">
+            <div className="row">
+              <div className="col-md-1 item-index">
+                {this.props.index + 1}
+              </div>
+              <div className="col-md-1 item-vote">
+                ⇧ <br/>
+                {this.props.item.score} <br/>
+                ⇩ <br/>
+              </div>
             </div>
           </div>
+          <div className="col-md-1">
+            {this.renderPreview()}
+          </div>
+          <div className="col-md-9 text-xs-left">
+            <h5>
+              <a href={this.props.item.url}>{this.props.item.title}</a> <small>({this.props.item.domain})</small>
+            </h5>
+            {this.renderExpander()}
+            <p className="small clearfix">
+              submitted <TimeAgo date={this.props.item.created_utc * 1000} /> ago by {this.props.item.author} to <Link to={`/r/${this.props.item.subreddit}`}>/r/{this.props.item.subreddit}</Link>
+              <br/>
+              <strong>{this.props.item.num_comments} Comments</strong>
+            </p>
+            {this.renderExpanded()}
+          </div>
         </div>
-
-        <div className="col-md-1">
-          {this.renderPreview()}
-        </div>
-        <div className="col-md-9 text-xs-left">
-          <h5>
-            <a href={this.props.item.url}>{this.props.item.title}</a> <small>({this.props.item.domain})</small>
-          </h5>
-          {this.renderExpander()}
-          <p className="small clearfix">
-            submitted <TimeAgo date={this.props.item.created_utc * 1000} /> ago by {this.props.item.author} to <Link to={`/r/${this.props.item.subreddit}`}>/r/{this.props.item.subreddit}</Link>
-            <br/>
-            <strong>{this.props.item.num_comments} Comments</strong>
-          </p>
-          {this.renderExpanded()}
-        </div>
-      </div>
+      </ReactCSSTransitionGroup>
     );
   }
 }
